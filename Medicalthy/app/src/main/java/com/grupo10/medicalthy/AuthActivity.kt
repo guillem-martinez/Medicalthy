055b8a0281700c2eeb4c80fb6a7fdcee0553419f
Cristianperezd <79981083+Cristianperezd@users.noreply.github.com>
2022-04-06 19:30:27 +0200

    Adaptación register(leer descripción del commit)
    He creado la función dentro de la misma clase como hemos hablado, pero al intentar separar lo del If no es posible, ya que al hacer el createUserWithEmailAndPassword, se utiliza addOnCompleteListener, que dentro de ese bloque no puede haber ningún tipo de return por lo que imposibiliza hacer como lo teniamos pensado.

--- Medicalthy/app/src/main/java/com/grupo10/medicalthy/AuthActivity.kt
+++ Medicalthy/app/src/main/java/com/grupo10/medicalthy/AuthActivity.kt
@@ -2,6 +2,7 @@ package com.grupo10.medicalthy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
@@ -10,7 +11,6 @@ import kotlinx.android.synthetic.main.activity_auth.*
 class AuthActivity : AppCompatActivity() {
 
     val database = FirebaseAuth.getInstance()
-    val authobject = Auth() ;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
@@ -23,10 +23,14 @@ class AuthActivity : AppCompatActivity() {
         title = getString(R.string.authTitle)
 
         signUpButton.setOnClickListener {
-            authobject.login(email.text.toString(),password.text.toString(), database, this);
+
+
+            register(email,password,database)
 
             /*
             if(email.text.isNotEmpty() && password.text.isNotEmpty()){
+
+
                 database.createUserWithEmailAndPassword(email.text.toString(),password.text.toString()).addOnCompleteListener {
 
                     if(it.isSuccessful){
@@ -37,17 +41,49 @@ class AuthActivity : AppCompatActivity() {
                     }
                 }
             }
-
             */
+
         }
     }
 
-    private fun showAlert(){
+
+    /*
+    Funcion que nos permite mostrar el mensaje de error que le pasemos como parametro
+     */
+    private fun showAlert(errorMessage:String){
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Error")
-        builder.setMessage("Se ha producido un error autenticando al usuario.")
+        builder.setMessage(errorMessage)
         builder.setPositiveButton("Aceptar", null)
         val dialog : AlertDialog = builder.create()
         dialog.show()
+    }
+
+
+    /*
+    A través de el email y password pasado por parametro se registra a este usuario en la database:FirebaseAuth
+     */
+
+    private fun register(email:EditText, password:EditText, database:FirebaseAuth): Boolean {
+
+        if(email.text.isNotEmpty() && password.text.isNotEmpty()){
+
+
+            database.createUserWithEmailAndPassword(email.text.toString(),password.text.toString()).addOnCompleteListener {
+
+                if(it.isSuccessful){
+                    R.layout.activity_home
+                }
+                else{
+                    showAlert("Se ha producido un error autenticando al usuario.")
+                }
+            }
+        }else
+        {
+            showAlert("Correo i/o electrónico vacios, porfavor ingrese el texto restante.")
+        }
+
+
+        return true;
     }
 }
