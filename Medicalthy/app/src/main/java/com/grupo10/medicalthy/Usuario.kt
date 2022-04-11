c04a05008c30e0354293e58832e9a05ce924e19a
ruubeensf <rubensf1031@gmail.com>
2022-04-11 12:54:30 +0200

    creacion clase usuario

---
+++ Medicalthy/app/src/main/java/com/grupo10/medicalthy/Usuario.kt
@@ -0,0 +1,11 @@
+package com.grupo10.medicalthy
+
+class Usuario(var Edad: Int) {
+
+    //private var username?
+    //private var nombre?
+    //private var apellido?
+    //public var edad = Edad
+    //private var pacientes:List<Usuario>
+
+}
