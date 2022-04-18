00996a7def4fd1d94f9425068a32f190cfa08ea9
MellacE98 <mellace1998@gmail.com>
2022-04-18 18:50:01 +0200

    Añadida la API de OCR
    Ahora cuando se toma una fotografia con la camara,  se envia a la API de OCR para que extraiga el texto, las imagenes que tenemos actualmente no permiten que se extraiga el texto directamente, se tendrian que tomar fotografias desde más cerca.

--- Medicalthy/app/src/main/java/com/grupo10/medicalthy/TakePictureActivity.kt
+++ Medicalthy/app/src/main/java/com/grupo10/medicalthy/TakePictureActivity.kt
@@ -1,22 +1,32 @@
 package com.grupo10.medicalthy
 
 import android.Manifest
+import android.net.Uri
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import kotlinx.android.synthetic.main.activity_take_pic.*
 
+
 class TakePictureActivity : AppCompatActivity() {
+
+    val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_take_pic)
 
         button.isEnabled = false
-
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 111)
         } else {
@@ -31,11 +41,25 @@ class TakePictureActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == 101) {
-            var pic = data?.getParcelableExtra<Bitmap>("data")
-            // Aqui se puede guardar la imagen a la base de datos y enviar a la api
-            imageView.setImageBitmap(pic)
+        if (resultCode == RESULT_OK && requestCode == 101 && data != null){
+            /* val uri = applicationContext.drawableToUri(R.drawable.captura)
+            imageView.setImageURI(null)
+            imageView.setImageURI(uri)
+            val bitmap: Bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri) */
+            imageView.setImageBitmap(data.extras?.get("data") as Bitmap)
+            val image = InputImage.fromBitmap(data.extras?.get("data") as Bitmap, 0)
+            val result = recognizer.process(image)
+                .addOnSuccessListener { visionText ->
+                    for (block in visionText.textBlocks) {
+                        val text = block.text
+                        print(text)
+                    }
+                }
         }
+    }
+
+    fun Context.drawableToUri(drawable: Int):Uri{
+        return Uri.parse("android.resource://$packageName/$drawable")
     }
 
     override fun onRequestPermissionsResult(
