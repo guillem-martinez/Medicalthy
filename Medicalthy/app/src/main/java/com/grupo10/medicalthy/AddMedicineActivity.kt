487bb629dc777fc9727a3b9952408f3b931c03b2
Cristianperezd <79981083+Cristianperezd@users.noreply.github.com>
2022-05-01 13:08:25 +0200

    Añadir foto del medicamento(Work in Progress)
    Esta en la actividad AddMedicine que viene del botón de añadir medicina, pero luego habrá que cambiarlo.

--- Medicalthy/app/src/main/java/com/grupo10/medicalthy/AddMedicineActivity.kt
+++ Medicalthy/app/src/main/java/com/grupo10/medicalthy/AddMedicineActivity.kt
@@ -1,17 +1,187 @@
 package com.grupo10.medicalthy
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.icu.text.SimpleDateFormat
+import android.net.Uri
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.IOException
+import java.util.*
 
 class AddMedicineActivity : AppCompatActivity() {
+
+    lateinit var selectedImage: ImageView
+    lateinit var cameraBtn: Button
+    lateinit var currentPhotoPath: String
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_medicine)
+
 
         setup()
     }
 
     private fun setup() {
         title = getString(R.string.app_name)
+
+        selectedImage = findViewById(R.id.displayImageView)
+        cameraBtn = findViewById(R.id.cameraBtn)
+
+        cameraBtn.setOnClickListener(View.OnClickListener { view ->
+
+            //.makeText(this, "Notificación corta", Toast.LENGTH_SHORT).show()
+            askCameraPermissions()
+
+
+        })
+
+
     }
+
+    private fun askCameraPermissions() {
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA) , 101)
+
+
+
+
+        }else {
+
+            Toast.makeText(this, "Notificación askCameraPermissions", Toast.LENGTH_SHORT).show()
+
+
+            dispatchTakePictureIntent()
+        }
+
+    }
+
+    /*
+    private fun openCamera() {
+        var camera = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(camera, 102)
+
+    }*/
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 102 ){
+
+            Toast.makeText(this, "Notificación onActivtyResult", Toast.LENGTH_SHORT).show()
+
+            /*
+            val imageBitmap = data?.extras?.get("data") as Bitmap?
+            selectedImage.setImageBitmap(imageBitmap)
+
+             */
+
+            val f = File(currentPhotoPath)
+            selectedImage.setImageURI(Uri.fromFile(f))
+
+
+        }
+
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 101){
+            if(grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                Toast.makeText(this, "Notificación onRequestPermissionsResult", Toast.LENGTH_SHORT).show()
+                Log.d("tag", "hola")
+
+                dispatchTakePictureIntent()
+
+
+            }else{
+
+                showAlert(getString(R.string.takePicPermission))
+
+            }
+        }
+
+    }
+
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    val REQUEST_TAKE_PHOTO = 1
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.grupo10.android.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+                }
+            }
+        }
+    }
+
+
+
+
+    private fun showAlert(errorMessage : String){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(getString(R.string.simpleErrorMessage))
+        builder.setMessage(errorMessage)
+        builder.setPositiveButton(getString(R.string.acceptMessage), null)
+        val dialog : AlertDialog = builder.create()
+        dialog.show()
+    }
+
+
 }
