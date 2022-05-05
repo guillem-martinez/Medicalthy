e26834578b08d032619e6eef9457ba4a2a5270ce
estufamagica <57752904+estufamagica@users.noreply.github.com>
2022-05-05 18:36:53 +0200

    User tests push

---
+++ Medicalthy/app/src/test/java/com/grupo10/medicalthy/UserUnitTest.kt
@@ -0,0 +1,18 @@
+package com.grupo10.medicalthy
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class UserUnitTest {
+    var productlist= mutableListOf<Product>()
+
+    val usertest = User("test", "a", "b", 1, false, productlist, null)
+
+    @Test
+    fun testEmptyNameExpectsError(){
+        usertest.añadirMedicamentos("", "123456789", 30, 2)
+
+    }
+
+}
