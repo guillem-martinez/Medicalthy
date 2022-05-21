ad1bad1e09e453188f0dc84de0b29f62da98a636
Sergi Molina <s.molinagrau@hotmail.com>
2022-05-22 00:20:38 +0200

    Añadido muestra de medicación de todos los planes de un usuario

--- Medicalthy/app/src/main/java/com/grupo10/medicalthy/RandomUtils.kt
+++ Medicalthy/app/src/main/java/com/grupo10/medicalthy/RandomUtils.kt
@@ -1,5 +1,8 @@
 package com.grupo10.medicalthy
 
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_show_shots.*
 import java.util.concurrent.atomic.AtomicInteger
 
 object RandomUtils {
@@ -9,4 +12,15 @@ object RandomUtils {
 
     fun dateFormatter(timeInMillis: Long): String =
         android.text.format.DateFormat.format("dd/MM/yyyy hh:mm:ss", timeInMillis).toString()
+
+
+    fun getMedicineName(cn: String, cback: (String)->Unit){
+        FirebaseFirestore.getInstance().collection("medicamentos").document(cn).get().addOnSuccessListener { med ->
+            if (med != null) {
+                cback(med.get("Nombre").toString())
+            } else {
+                cback("Error")
+            }
+        }
+    }
 }
