8589f5fd1d1b0ecf7b3b32b5bd8b68338048e69f
Sergi Molina <s.molinagrau@hotmail.com>
2022-05-22 00:52:32 +0200

    Carga el nombre y la descripción del medicamento que se le pasa al abrir la página

--- Medicalthy/app/src/main/java/com/grupo10/medicalthy/ShowMedicineActivity.kt
+++ Medicalthy/app/src/main/java/com/grupo10/medicalthy/ShowMedicineActivity.kt
@@ -1,12 +1,18 @@
 package com.grupo10.medicalthy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.grupo10.medicalthy.RandomUtils.getMedicineDescription
+import com.grupo10.medicalthy.RandomUtils.getMedicineName
 import kotlinx.android.synthetic.main.activity_show_medicine.*
 
 class ShowMedicineActivity : AppCompatActivity() {
 
     private var tts = TextToSpeechClass(this)
+    private var user: String = ""
+    private var cn: String = ""
+    private var plan: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,12 +25,28 @@ class ShowMedicineActivity : AppCompatActivity() {
     private fun setup() {
         title = getString(R.string.app_name)
 
+        val intent_obj: Intent = intent
+        cn = intent_obj.getStringExtra("cn").toString()
+        user = intent_obj.getStringExtra("user").toString()
+
+        if(cn != null){
+            getMedicineName(cn){ name ->
+                getMedicineDescription(cn) { desc ->
+                    setTextViewInfo(name, desc, "")
+                }
+            }
+        }
+
         btnSpeak.setOnClickListener {
             tts.speak("Escúchame una cosa viejo demente, o te tomas la pastilla o te vas con San Pedro.")
         }
-
     }
 
+    private fun setTextViewInfo(name: String, description: String, hours: String){
+        lblTitle.text = if(name != "") name else "Nombre no encontrado"
+        lblDescription.text = if(description != "") description else "Descripción no encontrada"
+        lblShotsList.text = "    * TOMA X [ 0 P ]"
+    }
 
     override fun onDestroy() {
         tts.shutdown()
