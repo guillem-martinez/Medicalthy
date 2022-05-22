0e00470de68bec6557e1b01995c3605ffa9df880
Sergi Molina <s.molinagrau@hotmail.com>
2022-05-22 02:31:50 +0200

    Si se pulsa botón Tomado/No tomado se añade al historial (sin verificación)

--- Medicalthy/app/src/main/java/com/grupo10/medicalthy/ShowMedicineActivity.kt
+++ Medicalthy/app/src/main/java/com/grupo10/medicalthy/ShowMedicineActivity.kt
@@ -3,9 +3,13 @@ package com.grupo10.medicalthy
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
 import com.grupo10.medicalthy.RandomUtils.getMedicineDescription
 import com.grupo10.medicalthy.RandomUtils.getMedicineName
 import kotlinx.android.synthetic.main.activity_show_medicine.*
+import java.util.*
+import kotlin.reflect.typeOf
 
 class ShowMedicineActivity : AppCompatActivity() {
 
@@ -26,7 +30,10 @@ class ShowMedicineActivity : AppCompatActivity() {
         title = getString(R.string.app_name)
 
         val intent_obj: Intent = intent
-        cn = intent_obj.getStringExtra("cn").toString()
+        val cn_plan = intent_obj.getStringExtra("cn_plan").toString()
+
+        cn = cn_plan.split(",")[0].toString()
+        plan = cn_plan.split(",")[1].toString()
         user = intent_obj.getStringExtra("user").toString()
 
         if(cn != null){
@@ -40,6 +47,29 @@ class ShowMedicineActivity : AppCompatActivity() {
         btnSpeak.setOnClickListener {
             tts.speak("Escúchame una cosa viejo demente, o te tomas la pastilla o te vas con San Pedro.")
         }
+
+        btnOk.setOnClickListener{ btnOkPressed() }
+        btnNoOk.setOnClickListener{ btnNoOkPressed() }
+    }
+
+    private fun btnOkPressed(){
+        var result = FirebaseFirestore.getInstance()
+            .collection("users")
+            .document(user)
+            .collection("Planes")
+            .document(plan)
+            .collection("Historial")
+            .document(Date().toString()).set(mapOf("tomado" to true))
+    }
+
+    private fun btnNoOkPressed(){
+        var result = FirebaseFirestore.getInstance()
+            .collection("users")
+            .document(user)
+            .collection("Planes")
+            .document(plan)
+            .collection("Historial")
+            .document(Date().toString()).set(mapOf("tomado" to false))
     }
 
     private fun setTextViewInfo(name: String, description: String, hours: String){
