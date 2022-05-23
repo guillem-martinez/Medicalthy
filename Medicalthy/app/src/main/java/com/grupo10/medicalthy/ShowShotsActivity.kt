38e8e8c2ff99c3c2c5e367ab3e2f25b8b08a0309
Sergi Molina <s.molinagrau@hotmail.com>
2022-05-24 00:35:01 +0200

    fix: Corrección de errores y compatibilidad con el formato definido en PlanMedicineActivity

--- Medicalthy/app/src/main/java/com/grupo10/medicalthy/ShowShotsActivity.kt
+++ Medicalthy/app/src/main/java/com/grupo10/medicalthy/ShowShotsActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.*
@@ -15,11 +16,18 @@ import java.util.*
 
 class ShowShotsActivity : AppCompatActivity() {
     var x : Int = 0
-    var email = "usuario1@gmail.com"
+    var email = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_shots)
+
+
+        val bundle = intent.extras
+        email = bundle?.get(getString(R.string.intentEmail)).toString()
+
+        if(email == "" || email == "null")
+            email = "email@gmail.com"
 
         setup()
 
@@ -65,22 +73,25 @@ class ShowShotsActivity : AppCompatActivity() {
             var text: String = ""
             var tag: String = ""
 
-            for(plan in planes){ //Por cada plan en la colección de planes
-                usuario.collection("Planes").document(plan.id).collection("Tomas").document("Horas").get().addOnSuccessListener { horas ->
-                    for(hora in horas.data!!.values){
-                        val str_seconds = hora.toString().substring(18, 28)
-                        val hour : String = SimpleDateFormat("HH:mm").format(Date(str_seconds.toLong() * 1000))
-
-                        getMedicineName(plan.get("CN").toString()){ name ->
-                            val cn = plan.get("CN").toString()
-                            vertical_layout.addView(makeNewTextView("$name -> $hour", cn + "," + plan.id))
-                        }
-                    }
-                }
-            }
-            if(planes.isEmpty){
-                vertical_layout.addView(makeNewTextView("No se ha encontrado ningún plan en tu usuario", "0,0"))
-            }
-        }
-    }
+            if(planes != null){
+                for(plan in planes){
+                    usuario.collection("Planes").document(plan.id).collection("Tomas").get().addOnSuccessListener { time_stamp_list ->
+                        for(time_stamp in time_stamp_list){
+                            Log.d("times_stamp.id --> ", time_stamp.id.toString())
+                            val hour: String = SimpleDateFormat("HH:mm").format(Date(time_stamp.id.toLong()))
+                            getMedicineName(plan.get("CN").toString()) { name ->
+                                val cn = plan.get("CN").toString()
+                                vertical_layout.addView(
+                                    makeNewTextView(
+                                        "$name -> $hour",
+                                        cn + "," + plan.id
+                                    )
+                                )
+                            }//getMedicineName() ->
+                        }//for(time_stam in time_stamp_list)
+                    }//usuario.collection("Planes") ... (time_stamp_list)
+                }//for(plan in planes)
+            }//if(planes != null)
+        }//usuario.collection. (planes).addOnSuccessListener
+    }//setPlanButtons
 }
