ca3670fafac93110762a15e3fd8ec455fe1d81ba
MellacE98 <60268786+MellacE98@users.noreply.github.com>
2022-05-24 17:28:49 +0200

    Cambio de libreria de test
    Cambiada la libreria de test de JUnit4 a JUnit5

--- Medicalthy/app/src/test/java/com/grupo10/medicalthy/AuthUnitTest.kt
+++ Medicalthy/app/src/test/java/com/grupo10/medicalthy/AuthUnitTest.kt
@@ -1,9 +1,8 @@
 package com.grupo10.medicalthy
 
 import android.widget.EditText
-import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
