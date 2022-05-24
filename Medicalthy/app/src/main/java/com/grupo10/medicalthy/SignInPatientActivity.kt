53f231f8889b38b81e528b5e12440b92cbc9c737
Sergi Molina <s.molinagrau@hotmail.com>
2022-05-24 19:33:58 +0200

    feat: se añade funcionalidad para agregar usuarios cuidados a la cuenta personal

--- Medicalthy/app/src/main/java/com/grupo10/medicalthy/SignInPatientActivity.kt
+++ Medicalthy/app/src/main/java/com/grupo10/medicalthy/SignInPatientActivity.kt
@@ -3,19 +3,26 @@ package com.grupo10.medicalthy
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_add_patient.*
 import kotlinx.android.synthetic.main.activity_sign_in.*
+import kotlinx.android.synthetic.main.activity_sign_in_patient.*
+import kotlin.math.sign
 
 class SignInPatientActivity : AppCompatActivity() {
 
     val GOOGLE_SIGN_IN = 10
     val database = FirebaseAuth.getInstance()
     val authObject = Auth()
+
+    var email: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,12 +31,89 @@ class SignInPatientActivity : AppCompatActivity() {
         //TODO: Si se pulsa 'atrás' se cierra la aplicación en lugar de ir a la anterior Activity
         //      Ha desaparecido la flecha de retroceso
 
+        val bundle = intent.extras
+        email = bundle?.get("email").toString()
+
+        if(email == "" || email == null || email == "null"){
+            email = "usuario1@gmail.com"
+        }
+
         setup()
     }
 
     private fun setup(){
+        title = "Vinculación de paciente"
 
-        title = getString(R.string.signInTitle)
+        button3.setOnClickListener {
+            var textEmail: String = signInEmailPatient.text.toString()
+            var textPassword: String = signInPasswordPatient.text.toString()
 
+            if((textEmail != "") && (textPassword != "") && (textEmail != email)){
+                val success = authObject.verifyCredentialsSignIn(textEmail, textPassword)
+
+                if(success[0] as Boolean){
+                    database.signInWithEmailAndPassword(textEmail, textPassword)
+                        .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                setResponsable(textEmail)    //Quien es su responsable (email)
+                                setCuidaA(textEmail)         //Encargado de cuidar a signInEmailPatient
+                            } else {
+                                showAlert(getString(R.string.authErrorMessage))
+                            }
+                        }
+                }//if(success[0] as Boolean)
+            }
+            else{
+                showAlert("Sabemos que te quieres mucho, pero no te puedes añadir a ti mismo")
+            }
+        }
     }
+
+    private fun showAlert(errorMessage : String){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(getString(R.string.simpleErrorMessage))
+        builder.setMessage(errorMessage)
+        builder.setPositiveButton(getString(R.string.acceptMessage), null)
+        val dialog : AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    override fun onBackPressed() {
+        android.app.AlertDialog.Builder(this).apply {
+            setTitle(getString(R.string.onBackDialogTitle_SignInPatientActivity))
+            setMessage(getString(R.string.onBackDialogMessage_SignInPatientActivity))
+
+            setPositiveButton(getString(R.string.yesMessage)) { _, _ ->
+                // Si pulsan si se cierra la app, LOCURA
+                super.onBackPressed()
+            }
+
+            setNegativeButton(getString(R.string.noMessage)){_, _ ->
+            }
+
+            setCancelable(true)
+        }.create().show()
+    }
+
+
+    private fun setResponsable(email_persona_cuidada: String){
+        //Quien es su responsable (email)
+        FirebaseFirestore.getInstance()
+            .collection("users")
+            .document(email_persona_cuidada)
+            .collection("responsable")
+            .document(email)
+            .set(hashMapOf("exists" to "true"))
+    }
+
+    private fun setCuidaA(email_persona_cuidada: String){
+        //Encargado de cuidar a signInEmailPatient
+        FirebaseFirestore.getInstance()
+            .collection("users")
+            .document(email)
+            .collection("cuida_a")
+            .document(email_persona_cuidada)
+            .set(hashMapOf("exists" to "true"))
+    }
+
 }
