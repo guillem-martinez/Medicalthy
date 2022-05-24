14b336a71f134bc605b8e111413058d36e170405
MellacE98 <60268786+MellacE98@users.noreply.github.com>
2022-05-24 23:24:51 +0200

    Cambiados los tipos de test
    Se ha cambiado la libreria con las que se testean las funciones de backend, se han cambiado los tests de TakePic a tests parametrizados.

--- Medicalthy/app/src/test/java/com/grupo10/medicalthy/UserUnitTest.kt
+++ Medicalthy/app/src/test/java/com/grupo10/medicalthy/UserUnitTest.kt
@@ -4,12 +4,12 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 
 class UserUnitTest {
-    var productlist= mutableListOf<Product>()
+    /*var productlist= mutableListOf<Product>()
 
     val usertest = User("test", "a", "b", 1, false, productlist, null)
 
     @Test
     fun testEmptyNameExpectsError(){
         usertest.añadirMedicamentos("", "123456789", 30, 2)
-    }
+    }*/
 }
