5ace1ebcc468e6ff61f51b92cbf97428c898566e
Sergi Molina <s.molinagrau@hotmail.com>
2022-05-29 11:55:25 +0200

    añadida interfaz sencilla para visualizar pastillero (controlado por HW)

---
+++ Arduino/InterfazPastillero_VB/Form1.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.btnTake = New System.Windows.Forms.Button()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.picRedLight = New System.Windows.Forms.PictureBox()
+        Me.picSpeaker = New System.Windows.Forms.PictureBox()
+        Me.picArrow = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.picRedLight, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picSpeaker, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picArrow, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 12
+        '
+        'btnTake
+        '
+        Me.btnTake.Enabled = False
+        Me.btnTake.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
+        Me.btnTake.Location = New System.Drawing.Point(12, 518)
+        Me.btnTake.Name = "btnTake"
+        Me.btnTake.Size = New System.Drawing.Size(500, 50)
+        Me.btnTake.TabIndex = 3
+        Me.btnTake.Text = "Tomar pastilla"
+        Me.btnTake.UseVisualStyleBackColor = True
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.BaudRate = 115200
+        '
+        'picRedLight
+        '
+        Me.picRedLight.BackColor = System.Drawing.SystemColors.Control
+        Me.picRedLight.Image = Global.InterfazPastillero_VB.My.Resources.Resources.luz_roja
+        Me.picRedLight.Location = New System.Drawing.Point(422, 12)
+        Me.picRedLight.Name = "picRedLight"
+        Me.picRedLight.Size = New System.Drawing.Size(90, 90)
+        Me.picRedLight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picRedLight.TabIndex = 7
+        Me.picRedLight.TabStop = False
+        '
+        'picSpeaker
+        '
+        Me.picSpeaker.BackColor = System.Drawing.SystemColors.Control
+        Me.picSpeaker.Image = Global.InterfazPastillero_VB.My.Resources.Resources.altavoz
+        Me.picSpeaker.Location = New System.Drawing.Point(12, 12)
+        Me.picSpeaker.Name = "picSpeaker"
+        Me.picSpeaker.Size = New System.Drawing.Size(90, 90)
+        Me.picSpeaker.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picSpeaker.TabIndex = 6
+        Me.picSpeaker.TabStop = False
+        '
+        'picArrow
+        '
+        Me.picArrow.BackColor = System.Drawing.Color.White
+        Me.picArrow.Image = Global.InterfazPastillero_VB.My.Resources.Resources.flecha
+        Me.picArrow.Location = New System.Drawing.Point(227, 362)
+        Me.picArrow.Name = "picArrow"
+        Me.picArrow.Size = New System.Drawing.Size(69, 84)
+        Me.picArrow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picArrow.TabIndex = 5
+        Me.picArrow.TabStop = False
+        Me.picArrow.Visible = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.InterfazPastillero_VB.My.Resources.Resources.rueda_pastillero
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(500, 500)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(524, 580)
+        Me.Controls.Add(Me.picRedLight)
+        Me.Controls.Add(Me.picSpeaker)
+        Me.Controls.Add(Me.picArrow)
+        Me.Controls.Add(Me.btnTake)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.picRedLight, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picSpeaker, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picArrow, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents btnTake As Button
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents picArrow As PictureBox
+    Friend WithEvents picSpeaker As PictureBox
+    Friend WithEvents picRedLight As PictureBox
+End Class
---
+++ Arduino/InterfazPastillero_VB/Form1.vb
@@ -0,0 +1,150 @@
+﻿Imports System.IO.Ports
+
+Public Class Form1
+    Private picture As Image
+    Private angle As Double
+    Private new_angle As Double
+    Private pastilla_tomada As Boolean
+    Private pastilla_detectada As Boolean
+
+    Private buzzer_time As Integer
+    Private led_time As Integer
+
+    Private lectura As String
+
+    Public Function RotateImage(ByRef image As Image, ByVal offset As PointF, ByVal angle As Decimal) As Bitmap
+        If image Is Nothing Then
+            Throw New ArgumentNullException("image")
+        End If
+        ''create a new empty bitmap to hold rotated image
+        'Dim rotatedBmp As Bitmap = New Bitmap(image.Width, image.Height)
+        Dim rotatedBmp As Bitmap = New Bitmap(500, 500)
+        'Dim rotatedBmp As Bitmap = New Bitmap(image)
+        rotatedBmp.SetResolution(image.HorizontalResolution, image.VerticalResolution)
+        ''make a graphics object from the empty bitmap
+        Dim g As Graphics = Graphics.FromImage(rotatedBmp)
+        ''Put the rotation point in the center of the image
+        g.TranslateTransform(offset.X, offset.Y)
+        ''rotate the image
+        g.RotateTransform(angle)
+        ''move the image back
+        ''g.TranslateTransform(-offset.X, -offset.Y)
+        ''draw passed in image onto graphics object
+        g.DrawImage(image, New PointF(-250, -250))
+        'g.DrawImage(image, offset)
+        Return rotatedBmp
+    End Function
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        angle = 22.5
+        new_angle = 22.5
+        picture = PictureBox1.Image
+        Timer1.Enabled = True
+
+        'PictureBox2.BackColor = Color.Transparent
+
+        SerialPort1.PortName = "COM6"
+        SerialPort1.Open()
+
+        pastilla_tomada = True
+        pastilla_detectada = True
+        buzzer_time = 0
+        led_time = 0
+        lectura = ""
+
+        PictureBox1.Image = RotateImage(picture, New PointF(250, 250), angle)
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If Not new_angle = angle Then
+            angle = angle + 0.5
+            PictureBox1.Image = RotateImage(picture, New PointF(250, 250), angle)
+
+            If angle >= new_angle Then
+                picArrow.Visible = True
+            Else
+                picArrow.Visible = False
+            End If
+        End If
+
+        If buzzer_time >= 0 Then
+            buzzer_time -= Timer1.Interval
+        Else
+            picSpeaker.Visible = False
+        End If
+
+        If led_time >= 0 Then
+            led_time -= Timer1.Interval
+        Else
+            picRedLight.Visible = False
+        End If
+
+        If Not lectura = "" Then
+            'decode()
+        End If
+
+
+        'Rotate the image by a further 1 degree.
+        'Me.angle = (Me.angle + 1) Mod 360
+
+        'Repaint the image.
+        Me.PictureBox1.Refresh()
+    End Sub
+
+    Private Sub decode()
+        If lectura.Contains("<") Then
+            If lectura.Length > 2 Then
+                If lectura.Substring(1, 1) = "M" Then
+                    'motor (write)
+                    new_angle += Convert.ToInt32(lectura.Substring(2, 2))
+                ElseIf lectura.Substring(1, 1) = "B" Then
+                    'buzzer (write)
+                    picSpeaker.Visible = True
+                    buzzer_time = lectura.Substring(2, 4)
+                ElseIf lectura.Substring(1, 1) = "L" Then
+                    'led (write)
+                    picRedLight.Visible = True
+                    led_time = lectura.Substring(2, 4)
+                ElseIf lectura.Substring(1, 1) = "T" Then
+                    'consulta pastillas tomadas (read)
+                    If btnTake.Enabled = true Then
+                        SerialPort1.Write("1")
+                    Else
+                        SerialPort1.Write("0")
+                    End If
+                ElseIf lectura.Substring(1, 1) = "C" Then
+                    'consulta pastilla en cubeta (read)
+                    If picArrow.Visible = False Then
+                        SerialPort1.Write("1")
+                    Else
+                        SerialPort1.Write("0")
+                    End If
+                End If
+            End If
+        End If
+
+        lectura = ""
+    End Sub
+
+    Private Sub picArrow_Click(sender As Object, e As EventArgs) Handles picArrow.Click
+        picArrow.Visible = False
+        btnTake.Enabled = True
+        pastilla_detectada = True
+
+    End Sub
+
+    Private Sub btnTake_Click(sender As Object, e As EventArgs) Handles btnTake.Click
+        btnTake.Enabled = False
+        pastilla_tomada = True
+
+    End Sub
+
+    Private Sub SerialPort1_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        CheckForIllegalCrossThreadCalls = False
+
+        lectura = SerialPort1.ReadLine()
+        Me.Text = lectura
+        decode()
+    End Sub
+End Class
