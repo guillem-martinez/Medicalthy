360db0a731ce969acd7a2568b43be126ba1ab65e
Sergi Molina <s.molinagrau@hotmail.com>
2022-05-29 11:54:41 +0200

    añadida comunicación con firebase

--- Arduino/ESP32_Medicalthy_Pastillero/ESP32_Medicalthy_Pastillero.ino
+++ Arduino/ESP32_Medicalthy_Pastillero/ESP32_Medicalthy_Pastillero.ino
@@ -1,13 +1,16 @@
 //BASADO EN: https://github.com/mobizt/Firebase-ESP-Client/blob/main/examples/Firestore/CommitDocument/SetUpdateDelete/SetUpdateDelete.ino
 
 #include <WiFi.h>
+#include <WiFiUdp.h>
 #include <Firebase_ESP_Client.h>
 #include <addons/TokenHelper.h>
+#include <NTPClient.h>
 #include "Credentials.h"
 
-// Define Firebase Data object
-FirebaseData fbdo;
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
 
+FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
 
@@ -25,37 +28,16 @@ void connectToWiFi(){
     Serial.println();
 }
 
-
-void setup()
-{
-    Serial.begin(115200);
-    
-    connectToWiFi();
-
-    Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
-
-    config.api_key = API_KEY;
-    auth.user.email = USER_EMAIL;
-    auth.user.password = USER_PASSWORD;
-
-    config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
-
-    fbdo.setResponseSize(2048);
-
-    Firebase.begin(&config, &auth);
-    Firebase.reconnectWiFi(true);
-}
-
 void sendCommand(char c){
     Serial.print('<');
     Serial.print(c);
-    Serial.print('>');
+    Serial.println('>');
 }
 void sendCommand(char c, int v){
     Serial.print('<');
     Serial.print(c);
     Serial.print(v);
-    Serial.print('>');
+    Serial.println('>');
 }
 
 void getNextHour(int& next_hour){
@@ -109,10 +91,88 @@ bool pastillasEnCubeta(){
 }
 
 
+unsigned long getNTPHour(){
+    while(!timeClient.update()){
+        timeClient.forceUpdate();
+    }
+
+    int hh = timeClient.getHours();
+    int mm = timeClient.getMinutes();
+    int ss = timeClient.getSeconds();
+    int dd = timeClient.getDay();
+
+    //Poner la siguiente fecha: https://www.epochconverter.com/ considerando día, mes y año, a las 00h00 (en SEGUNDOS)
+    unsigned long t = 1653782400 + ((hh + 1) * 3600) + (mm * 60) + ss;
+    
+    return t;
+}
+
+
+bool sendDataToFirebase(){
+    std::vector<struct fb_esp_firestore_document_write_t> writes;
+    struct fb_esp_firestore_document_write_t transform_write;
+    transform_write.type = fb_esp_firestore_document_write_type_transform;
+    transform_write.document_transform.transform_document_path = "test_HW/test_document";
+    struct fb_esp_firestore_document_write_field_transforms_t field_transforms;
+    field_transforms.fieldPath = "counter";
+    field_transforms.transform_type = fb_esp_firestore_transform_type_increment;
+    FirebaseJson values;
+    values.set("integerValue", "1"); // increase by 1
+    field_transforms.transform_content = values.raw();
+    transform_write.document_transform.field_transforms.push_back(field_transforms);
+    writes.push_back(transform_write);
+    if (Firebase.Firestore.commitDocumentAsync(&fbdo, FIREBASE_PROJECT_ID, "", writes, "")){
+        return 1;
+    }
+    else{
+        Serial.println(fbdo.errorReason());
+        return 0;
+    }
+}
+
+
+void setup()
+{
+    Serial.begin(115200);
+    
+    connectToWiFi();
+    
+    Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+
+    //Ver 'Credentials.h'
+    config.api_key = API_KEY;
+    auth.user.email = USER_EMAIL;
+    auth.user.password = USER_PASSWORD;
+
+    config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+
+    fbdo.setResponseSize(2048);
+    Firebase.begin(&config, &auth);
+    Firebase.reconnectWiFi(true);
+
+    timeClient.begin();
+    timeClient.setTimeOffset(3600);
+}
+
 void loop()
 {
+    unsigned long base_time_seconds = getNTPHour();
+
+    Serial.println(base_time_seconds);
+
     unsigned long millis_now = 0;
     unsigned long next_millis = millis() + 1000;
+
+    moverMotor(45);
+    delay(2000);
+    activarBuzzer(1000);
+    delay(2000);
+    activarLuz(-1, 1000);
+    delay(2000);
+    while(pastillasEnCubeta() == false) { delay(100); }  
+    delay(1000);
+    while(pastillasTomadas() == false) { delay(100); }  
+
 
     while(true){
         millis_now = millis();
@@ -120,7 +180,12 @@ void loop()
         {
             next_millis = millis() + 1000;
             
-            Serial.println("Test");
+            if(sendDataToFirebase()){
+                //Envío correcto
+            }
+            else{
+                //Fallo al enviar...                
+            }
         }
     }
 }
