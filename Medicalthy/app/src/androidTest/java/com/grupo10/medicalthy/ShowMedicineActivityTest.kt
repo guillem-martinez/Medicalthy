378fb3f99c4fdc685d04c73d7fedda8bcff05e7d
MellacE98 <mellace1998@gmail.com>
2022-05-30 00:55:23 +0200

    Primeros tests de ShowMedicine
    Primeros tests de la pantalla ShowMedicine

---
+++ Medicalthy/app/src/androidTest/java/com/grupo10/medicalthy/ShowMedicineActivityTest.kt
@@ -0,0 +1,93 @@
+package com.grupo10.medicalthy
+
+import android.content.Intent
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.RootMatchers
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import org.hamcrest.Matcher
+
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ShowMedicineActivityTest {
+    @Rule
+    @JvmField
+    var mActivityrule: ActivityTestRule<SignInActivity> = ActivityTestRule(SignInActivity::class.java, true, false)
+
+    @Before
+    fun setUp() {
+        val intent = Intent()
+        mActivityrule.launchActivity(intent)
+    }
+
+    @Test
+    fun testWithoutMedicines() {
+        val email = "test_no_med@gmail.com"
+        val password = "test_no_med"
+        Espresso.onView(ViewMatchers.withId(R.id.signInEmail)).perform(ViewActions.typeText(email))
+        Espresso.onView(ViewMatchers.withId(R.id.signInPassword)).perform(ViewActions.typeText(password))
+        Espresso.onView(ViewMatchers.withId(R.id.loginButton)).perform(ViewActions.click())
+        Espresso.closeSoftKeyboard()
+        Thread.sleep(1000)
+        Espresso.onView(ViewMatchers.withId(R.id.btnShowShots)).perform(ViewActions.click())
+        Thread.sleep(1000)
+        Espresso.onView(withText("Tu medicación")).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testWithMedicines() {
+        val email = "klk123@gmail.com"
+        val password = "klk123"
+        Espresso.onView(ViewMatchers.withId(R.id.signInEmail)).perform(ViewActions.typeText(email))
+        Espresso.onView(ViewMatchers.withId(R.id.signInPassword)).perform(ViewActions.typeText(password))
+        Espresso.onView(ViewMatchers.withId(R.id.loginButton)).perform(ViewActions.click())
+        Espresso.closeSoftKeyboard()
+        Thread.sleep(1000)
+        Espresso.onView(ViewMatchers.withId(R.id.btnShowShots)).perform(ViewActions.click())
+        Thread.sleep(1000)
+        Espresso.onView(withText("Ibuprofeno Kern Pharma -> 13:24")).check(matches(isDisplayed()))
+        Espresso.onView(withText("Tu medicación")).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testWithMedicinesClickMedicine() {
+        val email = "klk123@gmail.com"
+        val password = "klk123"
+        Espresso.onView(ViewMatchers.withId(R.id.signInEmail)).perform(ViewActions.typeText(email))
+        Espresso.onView(ViewMatchers.withId(R.id.signInPassword)).perform(ViewActions.typeText(password))
+        Espresso.onView(ViewMatchers.withId(R.id.loginButton)).perform(ViewActions.click())
+        Espresso.closeSoftKeyboard()
+        Thread.sleep(1000)
+        Espresso.onView(ViewMatchers.withId(R.id.btnShowShots)).perform(ViewActions.click())
+        Thread.sleep(1000)
+        Espresso.onView(withText("Tu medicación")).check(matches(isDisplayed()))
+        Espresso.onView(withText("Ibuprofeno Kern Pharma -> 13:24")).perform(ViewActions.click())
+        Thread.sleep(1000)
+        Espresso.onView(withText("Ibuprofeno Kern Pharma")).check(matches(isDisplayed()))
+        Espresso.onView(withText("Esto corresponde a la descripción del Ibuprofeno Kern Pharma")).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testWithoutMedicinesLongClick() {
+        val email = "test_no_med@gmail.com"
+        val password = "test_no_med"
+        Espresso.onView(ViewMatchers.withId(R.id.signInEmail)).perform(ViewActions.typeText(email))
+        Espresso.onView(ViewMatchers.withId(R.id.signInPassword)).perform(ViewActions.typeText(password))
+        Espresso.onView(ViewMatchers.withId(R.id.loginButton)).perform(ViewActions.click())
+        Espresso.closeSoftKeyboard()
+        Thread.sleep(1000)
+        Espresso.onView(ViewMatchers.withId(R.id.btnShowShots)).perform(ViewActions.longClick())
+        Thread.sleep(1000)
+        Espresso.onView(withText("Tu medicación")).check(matches(isDisplayed()))
+    }
+}
